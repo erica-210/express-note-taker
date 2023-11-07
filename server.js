b17fc9b92d1c9d9b494c6fa063e3d351cf355456
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-11-06 22:22:10 -0600

    fixed issues

--- server.js
+++ server.js
@@ -15,39 +15,37 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // acessing routes
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        ///error logging
-        if (err) throw err;
-        let dbData = JSON.parse(data);
-        //Returns new database
-        res.json(dbData)
-    });   
-})
+app.get("/api/notes", (req, res) => {
+  // fs.readFile("./db/db.json", (err, data) => {
+  //   if (err) throw err;
+  //   let dbData = JSON.parse(data);
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+  //   res.json(dbData);
+  // });
+  res.json(notesDB);
+});
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 // posting notes
-app.post("/public/notes", (req, res) => {
-  const newNote = req.body;
+app.post("/api/notes", (req, res) => {
+  const newNote = {
+    text: req.body.text,
+    title: req.body.title,
+    id: uuidv4(),
+  };
 
-  newNote.id = uuidv4();
+  notesDB.push(newNote);
 
-  db.push(newNote);
-
-  fs.writeFileSync("./db/db.json", JSON.stringify(db));
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
 
   res.json(db);
 });
 
 // to delete notes
-app.delete("/public/notes/:id", (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
   const newDb = notesDB.filter((note) => note.id !== req.params.id);
 
   fs.writeFileSync("./db/db.json", JSON.stringify(newDb));
@@ -55,7 +53,11 @@ app.delete("/public/notes/:id", (req, res) => {
   readFile.json(newDb);
 });
 
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+
 // calling the port
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`app listening at http://localhost:${PORT}`)
 );
